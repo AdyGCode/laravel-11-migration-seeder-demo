c10e9705e71a7aa0060ccbfa8f1ec25bb54116ef
Ady G <60425995+AdyGCode@users.noreply.github.com>
2024-05-15 12:25:31 +0800

    Update UserSeeder.php
    Use factory to seed the users

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -14,35 +14,66 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
+        // Create Admin user
+        User::factory()->create([
+            'name' => 'Admin',
+            'email' => 'admin@example.com',
+            'password' => "Password1",
+        ]);
+
+        // Additional Seed Users
         $seedUsers = [
             [
-                "name" => "Admin",
-                "email" => "admin@example.com",
-                "password" => "Password1",
-                "email_verified_at" => now(), // Do not require Admin to be verified before use
+                'name' => 'Adrian Gould',
+                'email' => 'adrian.gould@example.com',
+                'password' => "Password1",
             ],
-
-            // add additional users here as needed:
-            // [
-            //     "name" => "The Name Here",
-            //     "email" => "TheEmailAddress@here.example.com",
-            //     "password" => "SomePasswordFortesting",
-            //     "email_verified_at" => now(), // Optional line
-            // ],
-
+            [
+                'name' => 'Ivanna Vinn',
+                'email' => 'ivanna.vinn@example.com',
+            ],
+            [
+                'name' => 'Russ Round',
+                'email' => 'russ.hin-around@example.com',
+            ],
+            [
+                'name' => 'Chip Buttie',
+                'email' => 'chip.buttie@example.com',
+            ],
+            [
+                'name' => 'Annie Wun',
+                'email' => 'annie.wun@example.com',
+            ],
+            [
+                'name' => 'Andy Mann',
+                'email' => 'andy.mann@example.com',
+            ],
+            [
+                "name" => "April Schauer",
+                "email" => "April.Schauer@example.com",
+            ],
+            [
+                "name" => "Al K. Seltzer",
+                "email" => "Al.K.Seltzer@example.com",
+            ],
+            [
+                "name" => "Dee Sember",
+                "email" => "Dee.Sember@example.com",
+            ],
+            [
+                "name" => "Jo Kerr",
+                "email" => "Jo.Kerr@example.com",
+            ],
+            [
+                "name" => "Izzy Kidding",
+                "email" => "Izzy.Kidding@example.com",
+            ],
         ];
 
         foreach ($seedUsers as $seedUser) {
-            $seedUser['password'] = Hash::make($seedUser['password']);
-
-            $newUser = User::create($seedUser);
+            $newUser = User::factory()->create($seedUser);
+            // If required you may then work with $newUser to do things such as add roles
         }
-
-        // Add a test user using a factory but defining the name and email
-        // User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
 
         // Add ten more users using a factory
         // User::factory(10)->create();
